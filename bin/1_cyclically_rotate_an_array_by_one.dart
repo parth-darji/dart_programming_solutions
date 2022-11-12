f242f3600eb1b72c827b32d70db9829d79a1ac64
Parth Darji <parth.flutter@gmail.com>
2022-11-12 22:24:07 +0530

    solved: 1. cyclically rotate an array by one by using temp array

---
+++ bin/1_cyclically_rotate_an_array_by_one.dart
@@ -0,0 +1,23 @@
+// Program to cyclically rotate an array by one
+// Input:  [1, 2, 3, 4, 5]
+// Output: [5, 1, 2, 3, 4]
+
+void rotateArrayByOne(List array) {
+  List tempArray = [];
+
+  // add the last element to the array
+  tempArray.add(array[array.length - 1]);
+
+  // add other element in the array (except last element)
+  for (int i = 0; i < array.length - 1; i++) {
+    tempArray.add(array[i]);
+  }
+
+  // assign tempArray value to array
+  array = tempArray;
+
+  // empty the temp array to remove from memory
+  tempArray = [];
+
+  print(array);
+}
---
+++ bin/answers.dart
@@ -0,0 +1,5 @@
+import '1_cyclically_rotate_an_array_by_one.dart';
+
+void main() {
+  rotateArrayByOne([1, 2, 3, 4, 5]);
+}
